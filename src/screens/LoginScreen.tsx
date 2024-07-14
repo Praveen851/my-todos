ad3625f590fe0ac472ab62be2a69d69a1f1935c1
Praveen851-shopup <praveen.babu@shopup.org>
2024-07-14 16:13:37 +0530

    feat: add type for login screen and use props

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,10 +1,16 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+import { RootStackParamList } from '../navigation/RootStackParamList'
+import ScreenNames from '../utils/constants/ScreenNames'
 
-const LoginScreen = () => {
+
+type Props = NativeStackScreenProps<RootStackParamList, ScreenNames>
+
+const LoginScreen = ({ route}: Props) => {
   return (
     <View>
-      <Text>LoginScreen</Text>
+      <Text>{route.params?.title}</Text>
     </View>
   )
 }
