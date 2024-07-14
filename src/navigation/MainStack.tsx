0acf06d8b70adc975fdde149407e3fabbdf9b724
Praveen851-shopup <praveen.babu@shopup.org>
2024-07-14 15:46:38 +0530

    feat: consume root stack type in main stack

--- src/navigation/MainStack.tsx
+++ src/navigation/MainStack.tsx
@@ -2,13 +2,15 @@ import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import LoginScreen from '../screens/LoginScreen';
 import RegisterScreen from '../screens/RegisterScreen';
+import { RootStackParamList } from './RootStackParamList';
+import { ScreenNames } from '../utils/constants/ScreenNames';
 
 const MainStack = () => {
-    const Stack = createNativeStackNavigator();
+    const Stack = createNativeStackNavigator<RootStackParamList>();
   return (
     <Stack.Navigator>
-      <Stack.Screen name = "Login" component={LoginScreen}/>
-      <Stack.Screen name = "Register" component={ RegisterScreen}/>
+      <Stack.Screen initialParams={{ title: ScreenNames.Login}} name={ScreenNames.Login} component={LoginScreen}/>
+      <Stack.Screen initialParams={{ title: ScreenNames.Register}} name={ScreenNames.Register} component={ RegisterScreen}/>
     </Stack.Navigator>
   )
 }
