8b636fdfa936983dd6f897a24018016cc0725d6c
Praveen851-shopup <praveen.babu@shopup.org>
2024-07-15 15:42:28 +0530

    feat: add top image for login screen

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Image } from 'react-native'
 import React from 'react'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import { RootStackParamList } from '../navigation/RootStackParamList'
@@ -10,9 +10,27 @@ type Props = NativeStackScreenProps<RootStackParamList, ScreenNames>
 const LoginScreen = ({ route}: Props) => {
   return (
     <View>
+      <View style={styles.topImageContainer}>
+        <Image source={require('../../assets/topVector.png')} style={styles.topImage}/>
+      </View>
       <Text>{route.params?.title}</Text>
     </View>
   )
 }
 
 export default LoginScreen
+
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#f5f5f5',
+    flex: 1,
+  },
+  topImageContainer: {
+    height: 50,
+  },
+  topImage: {
+    width: "100%",
+    height: 140,
+  }
+})
