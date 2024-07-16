cf365ed622aee3201248d394a2d809e2de48dba4
Praveen851-shopup <praveen.babu@shopup.org>
2024-07-16 22:38:43 +0530

    feat: add icons at bottom tab

--- src/navigation/MainStack.tsx
+++ src/navigation/MainStack.tsx
@@ -1,14 +1,28 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import DashBoard from "../screens/DashBoardScreen";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 const MainStack = () => {
     const Tab = createBottomTabNavigator();
     return (
         <Tab.Navigator>
-            <Tab.Screen name="Home" component={DashBoard} />
-            <Tab.Screen name="Today" component={DashBoard} />
-            <Tab.Screen name="Overall" component={DashBoard} />
+            <Tab.Screen
+                name="Today"
+                component={DashBoard}
+                options={{
+                    tabBarIcon: () => <Ionicons name="today" size={24} />,
+                }}
+            />
+            <Tab.Screen
+                name="Overall"
+                component={DashBoard}
+                options={{
+                    tabBarIcon: () => (
+                        <Ionicons name="notifications-circle-sharp" size={32} />
+                    ),
+                }}
+            />
         </Tab.Navigator>
     );
 };
