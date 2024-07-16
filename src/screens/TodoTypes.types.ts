ee917287325b17913eec6624d7ad42e65e96340a
Praveen851-shopup <praveen.babu@shopup.org>
2024-07-17 00:12:18 +0530

    feat: add todo types

---
+++ src/screens/TodoTypes.types.ts
@@ -0,0 +1,103 @@
+export type ToDoType = {
+    title: string;
+    description: string;
+    dueDate: string;
+    time: string;
+    status: "completed" | "pending";
+};
+
+export type TodoListType = ToDoType[];
+
+export const todoList: TodoListType = [
+    {
+        title: "Task 1",
+        description: "Task 1 description",
+        dueDate: "10/10/2022",
+        time: "10:00",
+        status: "completed",
+    },
+    {
+        title: "Task 1",
+        description: "Task 1 description",
+        dueDate: "10/10/2022",
+        time: "10:00",
+        status: "pending",
+    },
+    {
+        title: "Task 1",
+        description: "Task 1 description",
+        dueDate: "10/10/2022",
+        time: "10:00",
+        status: "completed",
+    },
+    {
+        title: "Task 1",
+        description: "Task 1 description",
+        dueDate: "10/10/2022",
+        time: "10:00",
+        status: "completed",
+    },
+    {
+        title: "Task 1",
+        description: "Task 1 description",
+        dueDate: "10/10/2022",
+        time: "10:00",
+        status: "pending",
+    },
+    {
+        title: "Task 1",
+        description: "Task 1 description",
+        dueDate: "10/10/2022",
+        time: "10:00",
+        status: "completed",
+    },
+    {
+        title: "Task 1",
+        description: "Task 1 description",
+        dueDate: "10/10/2022",
+        time: "10:00",
+        status: "pending",
+    },
+    {
+        title: "Task 1",
+        description: "Task 1 description",
+        dueDate: "10/10/2022",
+        time: "10:00",
+        status: "pending",
+    },
+    {
+        title: "Task 1",
+        description: "Task 1 description",
+        dueDate: "10/10/2022",
+        time: "10:00",
+        status: "pending",
+    },
+    {
+        title: "Task 1",
+        description: "Task 1 description",
+        dueDate: "10/10/2022",
+        time: "10:00",
+        status: "pending",
+    },
+    {
+        title: "Task 1",
+        description: "Task 1 description",
+        dueDate: "10/10/2022",
+        time: "10:00",
+        status: "pending",
+    },
+    {
+        title: "Task 1",
+        description: "Task 1 description",
+        dueDate: "10/10/2022",
+        time: "10:00",
+        status: "pending",
+    },
+    {
+        title: "Task 1",
+        description: "Task 1 description",
+        dueDate: "10/10/2022",
+        time: "10:00",
+        status: "pending",
+    },
+];
