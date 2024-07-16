d2d38df39605026372bfde90ec946253f3c221a1
Praveen851-shopup <praveen.babu@shopup.org>
2024-07-16 17:08:15 +0530

    feat: add Loader while fetching user data

--- src/utils/context/AuthContext.tsx
+++ src/utils/context/AuthContext.tsx
@@ -1,6 +1,7 @@
 import React, { createContext, ReactNode, useEffect, useState } from "react";
 import { authenticate, getData, showToast, storeData } from "../helpers";
 import { LOGIN_KEY } from "../constants";
+import { ActivityIndicator, View } from "react-native";
 
 type AuthContextType = {
     user: string | null;
@@ -19,23 +20,27 @@ interface AuthProviderProps {
 }
 
 const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+    const [user, setUser] = useState<string>("");
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
     useEffect(() => {
         isLoggedIn();
     }, []);
 
     const isLoggedIn = async () => {
         try {
+            setIsLoading(true);
             let userToken = await getData(LOGIN_KEY);
             if (userToken) setUser(userToken);
         } catch (err) {
             showToast("Something went wrong. Please try again", "error");
+        } finally {
+            setIsLoading(false);
         }
     };
-    const [user, setUser] = useState<string>("");
 
     const login = async (email: string, password: string) => {
         const isValidUser = await authenticate(email, password);
-        console.log(isValidUser, email, password, "checkAuth");
 
         if (isValidUser) {
             setUser(email);
@@ -45,9 +50,22 @@ const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
     };
 
     const logout = async () => {
-        setUser("");
         await storeData(LOGIN_KEY, "");
+        setUser("");
     };
+    if (isLoading) {
+        return (
+            <View
+                style={{
+                    justifyContent: "center",
+                    flex: 1,
+                    alignItems: "center",
+                }}
+            >
+                <ActivityIndicator size="large" />
+            </View>
+        );
+    }
 
     return (
         <AuthContext.Provider value={{ user, login, logout }}>
