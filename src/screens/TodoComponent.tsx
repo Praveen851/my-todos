591a3e3d0ea3cfe703fee4a99ac05624725ebbcc
Praveen851-shopup <praveen.babu@shopup.org>
2024-07-17 00:20:34 +0530

    feat: add date picker

--- src/screens/TodoComponent.tsx
+++ src/screens/TodoComponent.tsx
@@ -1,9 +1,32 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Platform, Pressable, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
 import { ToDoType } from "./TodoTypes.types";
 import Ionicons from "@expo/vector-icons/Ionicons";
 
 const ListTodoComponent = ({ title, status }: ToDoType) => {
+    const [currDate, setCurrDate] = useState<string>("today");
+    const [date, setDate] = useState<Date>(new Date());
+    const [showPicker, setShowPicker] = useState<boolean>(false);
+    const toggleDatePicker = () => setShowPicker(!showPicker);
+    const onChange: (
+        {
+            type,
+        }: {
+            type: any;
+        },
+        selectedDate: any
+    ) => void = ({ type }, selectedDate) => {
+        if (type == "set") {
+            const currentDate = selectedDate;
+            setDate(currentDate);
+            if (Platform.OS === "android") {
+                toggleDatePicker();
+                setCurrDate(currentDate.toDateString());
+            }
+        } else {
+            toggleDatePicker();
+        }
+    };
     return (
         <View style={styles.container}>
             <View style={styles.card}>
@@ -14,6 +37,13 @@ const ListTodoComponent = ({ title, status }: ToDoType) => {
                     <Ionicons name="checkbox" size={24} color={"green"} />
                 )}
                 <Text style={styles.title}>{title}</Text>
+                <Pressable style={styles.date} onPress={toggleDatePicker}>
+                    <Text>{currDate}</Text>
+                </Pressable>
+
+                <Pressable style={styles.view} onPress={() => {}}>
+                    <Text style={{ color: "blue" }}>View</Text>
+                </Pressable>
             </View>
         </View>
     );
@@ -25,4 +55,6 @@ const styles = StyleSheet.create({
     container: { margin: 10, borderWidth: 1, padding: 15 },
     card: { flexDirection: "row", alignItems: "center" },
     title: { marginLeft: 10 },
+    date: { position: "absolute", right: 40 },
+    view: { position: "absolute", right: 0 },
 });
