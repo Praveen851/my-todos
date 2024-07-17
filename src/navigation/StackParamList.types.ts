38df7d0e3f67fbc914a12ad6b1c62b23b4be920c
Praveen851-shopup <praveen.babu@shopup.org>
2024-07-17 14:29:44 +0530

    feat: add type for main and tab stack

--- src/navigation/StackParamList.types.ts
+++ src/navigation/StackParamList.types.ts
@@ -2,3 +2,13 @@ export type AuthStackParamList = {
     Login: undefined;
     Register: undefined;
 };
+
+export type MainStackParamList = {
+    DashBoard: undefined;
+    EditScreen: undefined;
+};
+
+export type TabStackParamList = {
+    TodayScreen: undefined;
+    OverAllScreen: undefined;
+};
