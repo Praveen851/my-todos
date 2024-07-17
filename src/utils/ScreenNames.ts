0a6876c424747e3b23b9fc365c2df723aa3bf661
Praveen851-shopup <praveen.babu@shopup.org>
2024-07-17 14:29:29 +0530

    feat: add screennames of main and tab stack

--- src/utils/ScreenNames.ts
+++ src/utils/ScreenNames.ts
@@ -2,3 +2,13 @@ export enum AuthScreenNames {
     Login = "Login",
     Register = "Register",
 }
+
+export enum MainScreenNames {
+    DashBoard = "DashBoard",
+    EditScreen = "EditScreen",
+}
+
+export enum TabScreenNames {
+    TodayScreen = "TodayScreen",
+    OverAllScreen = "OverAllScreen",
+}
