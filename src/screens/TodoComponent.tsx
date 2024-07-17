b2efc898fad104f8a99f75be7cb70571ab45fdb7
Praveen851-shopup <praveen.babu@shopup.org>
2024-07-17 17:24:12 +0530

    feat: remove date picker from list todo

--- src/screens/TodoComponent.tsx
+++ src/screens/TodoComponent.tsx
@@ -1,15 +1,7 @@
-import {
-    Alert,
-    Platform,
-    Pressable,
-    StyleSheet,
-    Text,
-    View,
-} from "react-native";
-import React, { useState } from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import React from "react";
 import { ToDoType } from "./TodoTypes.types";
 import Ionicons from "@expo/vector-icons/Ionicons";
-import DateTimePicker from "@react-native-community/datetimepicker";
 import { useNavigation } from "@react-navigation/native";
 import { MainScreenNames } from "../utils/ScreenNames";
 import { MainStackParamList } from "../navigation/StackParamList.types";
@@ -22,29 +14,6 @@ type NavigationProps = NativeStackScreenProps<
 
 const TodoComponent = ({ title, status, description, dueDate }: ToDoType) => {
     const navigation: NavigationProps["navigation"] = useNavigation();
-    const [currDate, setCurrDate] = useState<string>("today");
-    const [date, setDate] = useState<Date>(new Date());
-    const [showPicker, setShowPicker] = useState<boolean>(false);
-    const toggleDatePicker = () => setShowPicker(!showPicker);
-    const onChange: (
-        {
-            type,
-        }: {
-            type: any;
-        },
-        selectedDate: any
-    ) => void = ({ type }, selectedDate) => {
-        if (type == "set") {
-            const currentDate = selectedDate;
-            setDate(currentDate);
-            if (Platform.OS === "android") {
-                toggleDatePicker();
-                setCurrDate(currentDate.toDateString());
-            }
-        } else {
-            toggleDatePicker();
-        }
-    };
 
     const handleView = () => {
         navigation.navigate(MainScreenNames.EditToDoScreen, {
@@ -65,17 +34,14 @@ const TodoComponent = ({ title, status, description, dueDate }: ToDoType) => {
                     <Ionicons name="checkbox" size={24} color={"green"} />
                 )}
                 <Text style={styles.title}>{title}</Text>
-                <Pressable style={styles.date} onPress={toggleDatePicker}>
-                    <Text>{currDate}</Text>
-                </Pressable>
 
+                <View style={styles.date}>
+                    <Text>{dueDate}</Text>
+                </View>
                 <Pressable style={styles.view} onPress={handleView}>
                     <Text style={{ color: "blue" }}>Edit</Text>
                 </Pressable>
             </View>
-            {showPicker && (
-                <DateTimePicker mode="date" value={date} onChange={onChange} />
-            )}
         </View>
     );
 };
