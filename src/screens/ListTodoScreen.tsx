8176c155a6abc860c026f2b0cccb5d1f674daa6f
Praveen851-shopup <praveen.babu@shopup.org>
2024-07-18 16:18:32 +0530

    feat: Add state context

--- src/screens/ListTodoScreen.tsx
+++ src/screens/ListTodoScreen.tsx
@@ -1,62 +1,23 @@
 import { View, Text, FlatList, StyleSheet } from "react-native";
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext } from "react";
 import TodoComponent from "./TodoComponent";
-import { ToDoType } from "./TodoTypes.types";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { useNavigation } from "@react-navigation/native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { MainStackParamList } from "../navigation/StackParamList.types";
 import { MainScreenNames } from "../utils/ScreenNames";
-import { getTodoList, storeData } from "../utils/helpers";
-import { AuthContext } from "../utils/context/AuthContext";
+import { StateContext } from "../utils/context/StateContext";
 
 type NavigationProps = NativeStackScreenProps<
     MainStackParamList,
     MainScreenNames
 >;
+
 const ListTodoScreen = () => {
     const navigation: NavigationProps["navigation"] = useNavigation();
+    const { addTodo, toggleStatus, editTodo, deleteTodo, todoList } =
+        useContext(StateContext);
 
-    const [todoList, setTodoList] = useState<ToDoType[]>([]);
-    const { authTodoListKey } = useContext(AuthContext);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            const todos = await getTodoList(authTodoListKey);
-            setTodoList(todos);
-        };
-
-        fetchData();
-    }, []);
-
-    const updateTodoState = () => {
-        setTodoList([...todoList]);
-        storeData(authTodoListKey, JSON.stringify(todoList));
-    };
-
-    const addTodo = (todo: ToDoType) => {
-        todoList.unshift(todo);
-        updateTodoState();
-    };
-
-    const editTodo = (todo: ToDoType, id: string) => {
-        const index = todoList.findIndex((item) => item.id === id);
-        todoList[index] = todo;
-        updateTodoState();
-    };
-
-    const deleteTodo = (id: string) => {
-        const index = todoList.findIndex((item) => item.id === id);
-        todoList.splice(index, 1);
-        updateTodoState();
-    };
-
-    const toggleStatus = (id: string) => {
-        const index = todoList.findIndex((item) => item.id === id);
-        todoList[index].status =
-            todoList[index].status === "completed" ? "pending" : "completed";
-        updateTodoState();
-    };
     const handleCreateTodo = () => {
         navigation.navigate(MainScreenNames.CreateToDoScreen, {
             description: "",
