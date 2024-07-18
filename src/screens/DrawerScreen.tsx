774fb8aef1f7fb1037b6bce2a67e8dc2cea89a44
Praveen851-shopup <praveen.babu@shopup.org>
2024-07-18 12:21:17 +0530

    feat: add logout button in drawer

--- src/screens/DrawerScreen.tsx
+++ src/screens/DrawerScreen.tsx
@@ -1,12 +1,32 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Pressable } from "react-native";
 import React from "react";
+import ButtonStyles from "./LoginScreenStyles";
 
 const DrawerScreen = () => {
     return (
-        <View>
-            <Text>DrawerScreen</Text>
+        <View style={styles.container}>
+            <Pressable style={styles.buttonContainer}>
+                <Text style={ButtonStyles.buttonText}>Log out</Text>
+            </Pressable>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "flex-end",
+        alignItems: "center",
+    },
+    buttonContainer: {
+        flexDirection: "row",
+        paddingHorizontal: 32,
+        paddingVertical: 12,
+        marginHorizontal: 15,
+        borderRadius: 24,
+        backgroundColor: "black",
+        marginBottom: 24,
+    },
+});
 
 export default DrawerScreen;
