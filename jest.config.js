fac50d65bb48040844edbf4b941d4edbd7c29a9e
Praveen851-shopup <praveen.babu@shopup.org>
2024-07-20 02:47:40 +0530

    feat: update test setups

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,17 @@
 module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
-    moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
     transform: {
         "^.+\\.tsx?$": "ts-jest",
+        "^.+\\.(js|jsx)$": "babel-jest",
     },
     testMatch: ["**/?(*.)+(spec|test).[jt]s?(x)"],
-    globals: {
-        "ts-jest": {
-            tsconfig: "tsconfig.json",
-        },
+    transformIgnorePatterns: [
+        "/node_modules/(?!react-native-toast-message|other-modules-to-transform).+\\.js$",
+    ],
+    moduleNameMapper: {
+        "\\.(jpg|jpeg|png|gif)$": "<rootDir>/__mocks__/fileMock.js",
+        "\\.(css|less)$": "identity-obj-proxy",
     },
 };
