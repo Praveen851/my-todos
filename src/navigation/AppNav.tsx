c0f6355b1f2bd2b70af220e1a7dc89604b42f8db
Praveen851-shopup <praveen.babu@shopup.org>
2024-07-19 21:54:09 +0530

    feat: add notification to remind dues

--- src/navigation/AppNav.tsx
+++ src/navigation/AppNav.tsx
@@ -1,12 +1,53 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import Toast from "react-native-toast-message";
 import AuthStack from "./AuthStack";
 import { AuthContext } from "../utils/context/AuthContext";
 import MainStack from "./MainStack";
+import * as Notifications from "expo-notifications";
+import { StateContext } from "../utils/context/StateContext";
 
 const AppNav = () => {
     const { user } = useContext(AuthContext);
+    const { todoList } = useContext(StateContext);
+
+    const dueToday = todoList.filter(
+        (item) => item.dueDate === new Date().toDateString()
+    ).length;
+
+    const notificationMessage = () => {
+        return dueToday
+            ? `You have ${dueToday} task due today!`
+            : "You have no task due today! Add to track";
+    };
+    Notifications.setNotificationHandler({
+        handleNotification: async () => ({
+            shouldShowAlert: true,
+            shouldPlaySound: true,
+            shouldSetBadge: true,
+        }),
+    });
+
+    useEffect(() => {
+        const scheduleNotification = async () => {
+            await Notifications.requestPermissionsAsync();
+
+            await Notifications.cancelAllScheduledNotificationsAsync();
+
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: "Hello",
+                    body: `You have ${dueToday} task due today!`,
+                },
+                trigger: {
+                    hour: 8,
+                    minute: 0,
+                    repeats: true,
+                },
+            });
+        };
+        scheduleNotification();
+    }, []);
 
     return (
         <NavigationContainer>
